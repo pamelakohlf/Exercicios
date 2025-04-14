1b2ca0c7ac021423c2aa8affdd311100fbd394f4
pamelakohlf <pamelakohlf@gmail.com>
2025-04-13 20:30:03 -0400

    Create 4-pam.py

---
+++ Exercícios/4-pam.py
@@ -0,0 +1,12 @@
+### 4. Crie um programa que receba três números e mostre-os em ordem crescente.
+
+def ordem(num1,num2,num3):
+  numeros = [num1,num2,num3]
+  numeros.sort()
+  print(numeros)
+
+num1 = int(input("Digite o numero: "))
+num2 = int(input("Digite o numero: "))
+num3 = int(input("Digite o numero: "))
+
+ordem(num1,num2,num3)
